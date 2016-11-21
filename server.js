557d69a8fb28ebda866fe2cfc001c8c0dc41ca29
sanderlenaerts <sander.lenaerts@gmail.com>
2016-11-21 23:50:54 +0100

    Added CORS middleware

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Routes for the API are defined
 app.use('/api', require('./api/routes'));
