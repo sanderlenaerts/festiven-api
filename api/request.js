2673cc2ecf73696c768c621153e244e354a1e160
sanderlenaerts <sander.lenaerts@gmail.com>
2016-11-25 00:41:04 +0100

    [TRIAL] Added routing for sending requests

--- api/request.js
+++ api/request.js
@@ -32,20 +32,21 @@ module.exports.send = function(req, res, next){
             console.log(err);
           }
           console.log('Added to sent');
-        }
-      ).then(function(){
-        User.update(
-          {
-            _id: toUser._id
-          },
-          {$push: {received: fromUser._id}}, function(err){
-            if (err){
-              console.log(err);
+          User.update(
+            {
+              _id: toUser._id
+            },
+            {$push: {received: fromUser._id}}, function(err){
+              if (err){
+                console.log(err);
+              }
+              console.log('Added to received');
             }
-            console.log('Added to received');
-          }
-        ) // End update
-      }) // End then
+          ) // End update
+        } // End then
+        }
+      )
+
     }) // End find
   }) // End find
 }
