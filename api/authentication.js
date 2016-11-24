83468c065544a37be931eab825a566bf3113d91d
sanderlenaerts <sander.lenaerts@gmail.com>
2016-11-24 23:45:26 +0100

    [TRIAL] Added routing for sending requests

--- api/authentication.js
+++ api/authentication.js
@@ -15,11 +15,13 @@ module.exports.register = function(req, res, next){
       console.log(err);
     }
     else if (result){
+      console.log("User already created");
       // User already exists, so we do nothing
       res.status(200).json({'message': 'Account was already there'});
     }
     else {
       // Create account
+
 
       user.save(function(err){
         if (err){
@@ -27,7 +29,7 @@ module.exports.register = function(req, res, next){
           next(err);
         }
         else {
-
+          console.log("User registered");
           res.status(201).json({'message': 'Account was succesfully created'});
         }
       })
