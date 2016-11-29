b572f5ac58a3fe348f7ca8103c6d2612120a4c2c
sanderlenaerts <sander.lenaerts@gmail.com>
2016-11-29 19:15:18 +0100

    Added get friends routing

--- api/routes.js
+++ api/routes.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 var ctrlAuth = require('./authentication');
 var ctrlReq = require('./request');
+var ctrlUser = require('./user');
 
 // Example
 router.post('/register', ctrlAuth.register);
 router.post('/addrequest', ctrlReq.send);
-router.post('/user/sent', ctrlReq.getSent);
 
-router.get('/user/friends')
+
+router.post('/user/sent', ctrlUser.getSent);
+router.post('/user/friends', ctrlUser.getFriends);
 
 module.exports = router;
