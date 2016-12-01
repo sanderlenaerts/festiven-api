8da784e0dce2713f8f42e9e61e064ca4280d6c4c
sanderlenaerts <sander.lenaerts@gmail.com>
2016-12-01 15:09:40 +0100

    [TRIAL] Routing for add friends

--- api/request.js
+++ api/request.js
@@ -97,7 +97,7 @@ var removeRequests = function(fromId, toId, callback){
   console.log(toId);
   User.update(
     {_id: fromId},
-    {$pull: { received: {_id: toId}}},
+    {$pull: { "received": {"_id": toId}}},
     {safe: true},
     function(err, resultThree){
       if (err){
@@ -106,7 +106,7 @@ var removeRequests = function(fromId, toId, callback){
       console.log("Request removed from first user");
       User.update(
         {_id: toId},
-        {$pull: { sent: {_id: fromId}}},
+        {$pull: { "sent": {"_id": fromId}}},
         {safe: true},
         function(err, resultFour){
           if (err){
@@ -129,7 +129,7 @@ var addToFriends = function(fromId, toId){
   console.log(toId);
   User.update(
     {_id: toId},
-    { $addToSet: { friends: {_id: fromId}}},
+    { $addToSet: { "friends": {"_id": fromId}}},
     {safe: true},
     function(err, resultFour){
       if (err){
@@ -138,7 +138,7 @@ var addToFriends = function(fromId, toId){
       console.log("Friends was added to user one");
       User.update(
         {_id: fromId},
-        {$addToSet: { friends: {_id: toId}}},
+        {$addToSet: { "friends": {"_id": toId}}},
         {safe: true},
         function(err, resultFour){
           if (err){
