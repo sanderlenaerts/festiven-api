29983caacca3736202fe8a9fdc4421a4dce26e4d
sanderlenaerts <sander.lenaerts@gmail.com>
2016-12-01 14:31:39 +0100

    [TRIAL] Routing for add friends

--- api/request.js
+++ api/request.js
@@ -4,7 +4,6 @@ var User = mongoose.model('User');
 
 
 module.exports.send = function(req, res, next){
-  console.log('SEND REQUEST');
 
   var from = req.body.origin;
   var to = req.body.to;
@@ -51,4 +50,112 @@ module.exports.send = function(req, res, next){
 
     }) // End find
   }) // End find
+}
+
+module.exports.accept = function(req, res, next){
+  var from = req.body.from;
+  var id = req.body.id;
+
+  // Get the objects
+
+  findUsers(from, id, function(fromId, toId){
+    removeRequests(fromId, toId, function(fromId, toId){
+      addToFriends(fromId, toId);
+    });
+  })
+}
+
+var findUsers = function(from, to, callback){
+  console.log("Finding users");
+  var fromId = null;
+  var toId = null;
+
+  User.findOne({id: from}, function(err, resultOne){
+    if (err){
+      console.log(err);
+    }
+    console.log("Found first user");
+    fromId = resultOne._id;
+  });
+
+  User.findOne({id: to}, function(err, resultTwo){
+    if (err){
+      console.log(err);
+    }
+    console.log("Found second user");
+    toId = resultTwo._id;
+  });
+
+  callback(fromId, toId);
+}
+
+var removeRequests = function(fromId, toId, callback){
+  console.log("First callback was called");
+  console.log("Callback is removing request");
+  console.log(fromId);
+  console.log(toId);
+  User.update(
+    {_id: fromId},
+    {$pull: { received: {_id: toId}}},
+    {safe: true},
+    function(err, resultThree){
+      if (err){
+        console.log(err);
+      }
+      console.log("Request removed from first user");
+    });
+    User.update(
+      {_id: toId},
+      {$pull: { sent: {_id: fromId}}},
+      {safe: true},
+      function(err, resultFour){
+        if (err){
+          console.log(err);
+        }
+        console.log("Request removed from second user");
+      }
+    );
+
+    callback(fromId, toId);
+}
+
+var addToFriends = function(fromId, toId){
+  console.log("Second callback was called");
+  console.log("Callback is adding friends");
+  console.log(fromId);
+  console.log(toId);
+  User.update(
+    {_id: toId},
+    { $addToSet: { friends: {_id: fromId}}},
+    {safe: true},
+    function(err, resultFour){
+      if (err){
+        console.log(err);
+      }
+      console.log("Friends was added to user one");
+
+    }
+  );
+  User.update(
+    {_id: fromId},
+    {$addToSet: { friends: {_id: toId}}},
+    {safe: true},
+    function(err, resultFour){
+      if (err){
+        console.log(err);
+      }
+      console.log("Friends was added to user two");
+    })
+}
+
+
+module.exports.decline = function(req, res, next){
+  var from = req.body.origin;
+  var to = req.body.to;
+
+  findUsers(from, id, function(fromId, toId){
+    removeRequests(fromId, toId, function(fromId, toId){
+      // Nothing to do here.
+    });
+  })
 }
--- api/routes.js
+++ api/routes.js
@@ -8,6 +8,8 @@ var ctrlUser = require('./user');
 // Example
 router.post('/register', ctrlAuth.register);
 router.post('/addrequest', ctrlReq.send);
+router.post('/acceptrequest', ctrlReq.accept);
+router.post('/declinerequest', ctrlReq.decline);
 
 
 router.post('/user/sent', ctrlUser.getSent);
