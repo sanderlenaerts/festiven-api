0e1526752c75768f8e40d16853ba84ee3e872b94
sanderlenaerts <sander.lenaerts@gmail.com>
2016-12-04 12:46:20 +0100

    [TRIAL] Added socket.io

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+require('.socket');
 require('./db');
 var app = express();
 
---
+++ socket.js
@@ -0,0 +1,57 @@
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+var http = require('http');
+var request = require("request");
+
+var clients = {};
+
+io.sockets.on('connection', function (socket) {
+
+    // Emit connect event to the socket
+    // This event on client side will emit an event to the server (here) of storeClientInfo
+    console.log('Connection');
+    socket.emit('connect', {});
+
+    socket.on('storeClientInfo', function (data) {
+        console.log('Storing client info');
+
+        var clientInfo = new Object();
+        clientInfo.customId  = data.customId;
+        clientInfo.clientId  = socket.id;
+
+        console.log("fb-id: " + data.customId);
+
+        // Save the fb-ids in friends
+        request.post('http://localhost:3000/api/user/friends', form: {id: data.customId}, function(err,httpResponse,body){
+          console.log(body);
+          clientInfo.friends = [];
+
+          // For every friend, save the fb id in the clientinfo
+          body.forEach(function(item){
+            clientInfo.friends.push(item.id);
+          })
+        });
+
+        clients[data.customId] = clientInfo;
+
+        console.log(clients);
+    });
+
+    socket.on('add-friend', function(data){
+      // Push the new friend to the friends
+      socket.clientInfo.friends.push(data.id);
+    });
+
+    socket.on('disconnect', function (data) {
+
+      for (var key in clients) {
+        if (clients.hasOwnProperty(key)) {
+          if (clients[key].clientId = socket.id){
+            delete clients[key];
+            break;
+          }
+        }
+      }
+    });
+
+});
